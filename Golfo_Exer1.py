a45ae1b74552a98b4a86c3d7694f4c07c61a00ea
rcegolfo <36612345+rcegolfo@users.noreply.github.com>
2018-02-19 10:58:50 +0800

    Add files via upload

---
+++ Golfo_Exer1.py
@@ -0,0 +1,4 @@
+Money = input("How much money do you have? ")
+Iphone = 50000
+print "You can buy", Money/Iphone, "Iphone"
+print "You will be needing", Iphone-(Money%Iphone), "to buy an Iphone or another Iphone"
